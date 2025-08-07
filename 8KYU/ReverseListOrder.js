081d6b30059b7fcd0b8a819bedb2e6a981ea9c36
Deng Kuat <denq1738@gmail.com>
2025-08-07 16:39:49 +0200

    revered an array of integers using reverse array methog to 8KYU folder

---
+++ 8KYU/ReverseListOrder.js
@@ -0,0 +1,14 @@
+function reverseList(list) {
+    return list.reverse();
+}
+console.log(reverseList([1, 2, 3, 4]));
+
+//?input
+//we given a list of integers in array form
+
+//*info
+//reverse the order of integers in the list 
+
+//!test cases
+// [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// [9, 2, 0, 7]  -> [7, 0, 2, 9]
