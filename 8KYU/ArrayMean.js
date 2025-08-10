92c5ac6db58710216032d1073450d96c9951ebb8
Deng Kuat <denq1738@gmail.com>
2025-08-10 11:39:32 +0200

    array average using reduce to 8KYU folder

---
+++ 8KYU/ArrayMean.js
@@ -0,0 +1,18 @@
+function findAverage(nums) {
+    // Code here
+    return nums.reduce((acc, curr) => (acc+curr), 0)/nums.length;
+  }
+
+  //?input
+  //array of integers
+  //integers are positive
+
+  //*info
+  //average of all the integers in the array
+  //return value is intgers average
+
+  //!test cases
+    //   [1, 3, 5, 7] - 4
+
+    //steps
+    //array so- use reduce - a call back function that return a single digits of a result of the input array 
