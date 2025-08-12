f5561f706e4fd278806bd883254f792d912f64f5
Deng Kuat <denq1738@gmail.com>
2025-08-12 08:42:57 +0200

    alternative captalization using forEach to 7KYU folder

---
+++ 6KYU/TwoSum.js
@@ -0,0 +1,12 @@
+function twoSum(numbers, target) {
+    return [0, 1];
+}
+
+//?input
+//a function that takes an array of integers/numbers
+//the integers are from the test and 
+
+
+//*info
+
+//!test Cases
---
+++ 7KYU/AlternativeCaptilizaiton.js
@@ -0,0 +1,31 @@
+function capitalize(s){
+    const even = [];
+    const odd = [];
+    s.split('').forEach((char, index) => {
+        if(index%2===0){
+            even.push(char.toUpperCase());
+            odd.push(char);
+        } else{
+            even.push(char);
+            odd.push(char.toUpperCase());
+        }
+    });
+    return [even.join(''), odd.join('')];
+}
+
+  //?inout
+  //so we have an input of a string 
+  //the string is initially all lower case letters
+  //the input string has no spaces
+
+  //*info
+  //captalize the letter that occupies the even numbers and odd numbers separaly
+  //return as shown below
+  //index 0 will be considered even
+
+  //!test cases
+//   capitalize("abcdef") = ['AbCdEf', 'aBcDeF']
+
+//
+//iterate through the input string
+//then captalize the ones that are in the even index as vice versa for the other one
