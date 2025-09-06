07203f6e3b72bbcbaa799eec124f2de539982e7d
Deng Kuat <denq1738@gmail.com>
2025-09-06 15:26:37 +0200

    Interview Question 1, l used map and set

--- PracticeInterviewStyle/interview1.js
+++ PracticeInterviewStyle/interview1.js
@@ -1,9 +1,10 @@
 // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 2, 5];
 
 //find out the duplicate elements from the arr
-//ok l could create a Set this will remove the duplicates 
-//the l could use filter the array method 
-//to filter out elements in the array that are also in the set
+//so l sort the array so that the duplicates can be next to each other
+//the find out if the item we iterating thorugh and the next one are the same 
+//if they are the same then we push into an emtpty array we creating to hold all repeated values
+//so l filter out the duplicates then l return it as my final return
 
 function findDuplicate(arr){
    const sortedArray =  arr.sort();
