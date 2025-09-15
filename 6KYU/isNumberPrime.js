de1f7728adf20d73d46031f3a017fb2af6ed3597
Deng Kuat <denq1738@gmail.com>
2025-09-15 15:47:58 +0200

    isnumberprime l used if statements, forloop and built in Math.sqt methods to 6KYU folder

--- 6KYU/isNumberPrime.js
+++ 6KYU/isNumberPrime.js
@@ -11,7 +11,6 @@ function isPrime(num) {
  }
 
 
-
   //?input
   //so we are taking in an intger as a parameter 
   //it is a single integer
@@ -41,4 +40,14 @@ function isPrime(num) {
     so l could use an if statement so if a that interated integer if the input is divisible by the iterabted value and the solution has no reminder then we shall add it to our array that l shall create 
     so we could if the length of that array containing all the divisibles of input if more than 2 then that input is not prime else prime
     again if the intger is less than 1 then its definetly not a prime 
+
+    Why did my code time out?
+    Our servers are configured to only allow a certain amount of time for your code to execute. In rare cases the server may be taking on too much work and simply wasn't able to run your code efficiently enough. Most of the time though this issue is caused by inefficient algorithms. If you see this error multiple times you should try to optimize your code further.
+    Does it answer this question 
+
+    alternative 
+    since 1 is not prime, 2 is the only prime number we could have that in an if statement to limit and reduce the time coplexity
+    have our count as 2 since a prime only has 2 dvisibles
+    using math.sqt built in method
+   Math.sqrt() static method in JavaScript returns the square root of a number. It is a built-in function of the Math object
     */
