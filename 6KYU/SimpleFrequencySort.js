7d01b6972d671b1444bf25c225510ed199c3bb46
Deng Kuat <denq1738@gmail.com>
2025-11-11 16:46:14 +0200

    simple frequencysort

--- 6KYU/SimpleFrequencySort.js
+++ 6KYU/SimpleFrequencySort.js
@@ -6,7 +6,7 @@ console.log(solve([2,3,5,3,7,9,5,3,7]));
 console.log(solve([4,9,5,0,7,3,8,4,9,0]));
 
 //?input
-//an array of integers as usual
+//an array of integers as usual 
 
 //*info
 //so we will sort them in a decending order way and return that
