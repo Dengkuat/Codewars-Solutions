81794161f1b9e9e85772cc0e46b1565ff7165c62
Deng Kuat <denq1738@gmail.com>
2025-11-11 16:44:55 +0200

    Mean square error

---
+++ 6KYU/SimpleFrequencySort.js
@@ -0,0 +1,17 @@
+const solve = arr => {
+  //..
+  return arr.sort()
+}
+console.log(solve([2,3,5,3,7,9,5,3,7]));
+console.log(solve([4,9,5,0,7,3,8,4,9,0]));
+
+//?input
+//an array of integers as usual
+
+//*info
+//so we will sort them in a decending order way and return that
+
+//!test case
+// solve([2,3,5,3,7,9,5,3,7]) = [3,3,3,5,5,7,7,2,9]
+// -- We sort by highest frequency to lowest frequency.
+// -- If two elements have same frequency, we sort by increasing value.
